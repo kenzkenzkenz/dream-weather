cb79a4442e4258e5e103f16e25eae820e628615b
kenzkenzkenz <contact.kenzie@proton.me>
2026-01-11 01:07:01 -0600

    adds validation for user preferences

--- backend/src/main/java/com/dreamweather/backend/model/UserPrefs.java
+++ backend/src/main/java/com/dreamweather/backend/model/UserPrefs.java
@@ -1,12 +1,18 @@
 package com.dreamweather.backend.model;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class UserPrefs {
 	
+	@NotNull
 	private Country country;
+	
+	@NotNull
 	private String precipitation;
+	
+	@NotNull
 	private String temperature;
 
 }
